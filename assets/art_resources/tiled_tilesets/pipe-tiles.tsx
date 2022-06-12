b673189f58dc3fcddd17c8dfa96fc2904eb79ef9
DVD_B <dvd-b@outlook.de>
2022-06-12 18:38:24 +0200

    added more tilesets
    sandstone buildings, pipes, changed test-tower again, added more stuff to the example test-level2

---
+++ assets/art_resources/tiled_tilesets/pipe-tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="pipe-tiles" tilewidth="32" tileheight="32" tilecount="48" columns="12">
+ <image source="../tiles/pipe-tiles.png" width="384" height="128"/>
+</tileset>
