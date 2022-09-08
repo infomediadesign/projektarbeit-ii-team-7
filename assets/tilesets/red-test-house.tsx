770c777a7e1a68b1db4e6820fddd435266edc613
DVD_B <dvd-b@outlook.de>
2022-09-08 16:14:09 +0200

    i hate tiled editor so much
    moved files around after a failed .tmx to .json conversion.

--- assets/tilesets/red-test-house.tsx
+++ assets/tilesets/red-test-house.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.1" name="red-test-house" tilewidth="32" tileheight="32" tilecount="135" columns="15">
- <image source="sprite and tilesheets/test_house.png" width="511" height="288"/>
+ <image source="../tiles/test_house.png" width="511" height="288"/>
 </tileset>
