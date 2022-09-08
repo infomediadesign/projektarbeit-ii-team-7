208db937c09dd85b51b9b51fa6ccf292d0259ce6
DVD_B <dvd-b@outlook.de>
2022-09-08 16:14:48 +0200

    fixed .tsx files

--- assets/tilesets/test-house.tsx
+++ assets/tilesets/test-house.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.1" name="red-test-house" tilewidth="32" tileheight="32" tilecount="135" columns="15">
- <image source="sprite and tilesheets/test_house.png" width="511" height="288"/>
+ <image source="../tiles/test_house.png" width="511" height="288"/>
  <tile id="46" probability="0"/>
 </tileset>
--- assets/tilesets/test-tower.tsx
+++ assets/tilesets/test-tower.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.1" name="test-tower" tilewidth="32" tileheight="32" tilecount="70" columns="14">
- <image source="sprite and tilesheets/test_tower.png" width="448" height="160"/>
+<tileset version="1.9" tiledversion="1.9.1" name="test-tower" tilewidth="32" tileheight="32" tilecount="84" columns="14">
+ <image source="../tiles/test_tower.png" width="448" height="192"/>
 </tileset>
