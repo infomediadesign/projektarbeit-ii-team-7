88a7bc810338e4371f0eadc22e96a6458e14b971
DVD_B <dvd-b@outlook.de>
2022-09-13 10:51:42 +0200

    added tons of assets
    Level assets, GUI assets, .json files

--- assets/tilesets/test-ground.tsx
+++ assets/tilesets/test-ground.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.1" name="test-ground" tilewidth="32" tileheight="32" tilecount="24" columns="6">
- <image source="sprite and tilesheets/test_ground.png" width="192" height="128"/>
+ <image source="../tiles/test_ground.png" width="192" height="128"/>
 </tileset>
